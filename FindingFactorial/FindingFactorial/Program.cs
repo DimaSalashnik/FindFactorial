274520c533fc4041e1d18a08e81287d209140108
DimaSalashnik <47631950+DimaSalashnik@users.noreply.github.com>
2019-05-09 18:34:54 +0300

    v1.0 Console Edition
    Finding factorial numbers. Only 31 numbers due to limitations in int32 length.

---
+++ FindingFactorial/FindingFactorial/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindingFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process();
+        }
+
+        static int Fact(int n)
+        {
+            if (n == 1) return 1;
+            else return n * Fact(n - 1);
+        }
+
+        static void Process()
+        {
+            Console.WriteLine("Введите число для нахождения его факториала: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Факториал числа {num} = {Fact(num)}");
+            RepeatOperation();
+        }
+
+        static void RepeatOperation()
+        {
+            Console.WriteLine("Продолжить? (да/нет)");
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "да" || answer == "yes") { Process(); Console.Clear(); }
+            else if (answer == "нет" || answer == "no") { Console.WriteLine("Завершение работы..."); return; }
+            else { Console.WriteLine("Повторите попытку..."); RepeatOperation(); }
+        }
+    }
+}
